f7cf5fa1e0ecd279985d8025f7a122e6db320652
nguyenthingocha1209-afk <nguyenthingocha1209@gmail.com>
2026-03-27 18:08:16 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 
-const app = express;
+const app = express();
 
 // đọc file trong public
 app.use(express.static(path.join(__dirname, "public")));
