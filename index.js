7c99a47c756ab522e5b5c6e5da1d9cac4ce3aa27
nguyenthingocha1209-afk <nguyenthingocha1209@gmail.com>
2026-03-27 17:58:51 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,10 @@
-const express = require('express');
-const app = express();
+const express = require("express");
+const app = express;
 
-const AFFILIATE_ID = "14354840000";
+// cho phép đọc file trong public
+app.use(express.static("public"));
 
-app.get('/go', (req, res) => {
-    const url = req.query.url;
-    const sub = req.query.sub || "fb";
-
-    if (!url) return res.send("Thiếu link");
-
-    const encoded = encodeURIComponent(url);
-
-    const final = `https://s.shopee.vn/an_redir?origin_link=${encoded}&affiliate_id=${AFFILIATE_ID}&sub_id=${sub}`;
-
-    res.redirect(final);
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log("Server chạy rồi");
 });
-
-app.get('/', (req, res) => {
-    res.send("Web affiliate đang chạy ngon 👍");
-});
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log("Server chạy rồi"));
