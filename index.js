e00e46ffc393fe53bafa65b8d813657d7c8a608f
nguyenthingocha1209-afk <nguyenthingocha1209@gmail.com>
2026-03-27 18:00:26 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require("express");
+const path = require("path");
+
 const app = express;
 
-// cho phép đọc file trong public
-app.use(express.static("public"));
+// trỏ đúng tới folder public
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-  console.log("Server chạy rồi");
+  console.log("Server chạy OK");
 });
